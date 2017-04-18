26b097857cdaab941b08bdbc906b58589b63a654
Frank Schubert <fmschubert@ieee.org>
2017-04-18 16:33:28 +0200

    new release

--- build-all-deb-packages.sh
+++ build-all-deb-packages.sh
@@ -16,12 +16,12 @@ rm -rf ~/packaging/libcdx
 mkdir -p ~/packaging/libcdx
 
 # get version from changlog file:
-cd ~/workspace/cdx/trunk/CPP
+cd ~/git/cdx/CPP
 LIBCDX_VERSION=$(dpkg-parsechangelog --show-field Version | sed  's/-.*//')
 
 echo libcdx-C++ version: $LIBCDX_VERSION
 
-cp -r ~/workspace/cdx/trunk/CPP ~/packaging/libcdx/libcdx_$LIBCDX_VERSION
+cp -r ~/git/cdx/CPP ~/packaging/libcdx/libcdx_$LIBCDX_VERSION
 cd ~/packaging/libcdx
 
 tar cfz libcdx_$LIBCDX_VERSION.orig.tar.gz libcdx_$LIBCDX_VERSION
@@ -31,14 +31,14 @@ DEB_BUILD_OPTIONS="parallel=4" nice debuild -us -uc
 # Build the package for the Python CDX library, python-cdx_1.1-1_all.deb:
 
 # get version:
-cd ~/workspace/cdx/trunk/python
+cd ~/git/cdx/python
 export PY_CDX_VERSION=$(dpkg-parsechangelog --show-field Version | sed  's/-.*//')
 
 echo libcdx-Python version: $PY_CDX_VERSION
 
 rm -rf ~/packaging/python-cdx
 mkdir -p ~/packaging/python-cdx
-cp -r ~/workspace/cdx/trunk/python ~/packaging/python-cdx/python-cdx_$PY_CDX_VERSION
+cp -r ~/git/cdx/python ~/packaging/python-cdx/python-cdx_$PY_CDX_VERSION
 cd ~/packaging/python-cdx
 tar czf python-cdx_$PY_CDX_VERSION.orig.tar.gz python-cdx_$PY_CDX_VERSION
 cd python-cdx_$PY_CDX_VERSION
@@ -54,12 +54,12 @@ mkdir -p ~/packaging/cdx-tools
 cd ~/packaging/cdx-tools
 
 # get version:
-cd ~/workspace/cdx/trunk/tools
+cd ~/git/cdx/tools
 CDX_TOOLS_VERSION=$(dpkg-parsechangelog --show-field Version | sed  's/-.*//')
 
 echo CDX tools version: $CDX_TOOLS_VERSION
 
-cp -r ~/workspace/cdx/trunk/tools ~/packaging/cdx-tools/cdx-tools_$CDX_TOOLS_VERSION
+cp -r ~/git/cdx/tools ~/packaging/cdx-tools/cdx-tools_$CDX_TOOLS_VERSION
 cd ~/packaging/cdx-tools
 tar cfz cdx-tools_$CDX_TOOLS_VERSION.orig.tar.gz cdx-tools_$CDX_TOOLS_VERSION
 cd cdx-tools_$CDX_TOOLS_VERSION/
